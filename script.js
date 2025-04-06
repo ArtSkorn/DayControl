bf489158c25804f899ad774063b5e9bb7e2eee9f
ArtSkorn <a.skornyakov123@gmail.com>
2025-04-06 19:39:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  const days = document.getElementById('days');
+const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
